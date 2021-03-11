e441cff1213b66df90320af8fc9544e0cfa83162
EC2 Default User <ec2-user@ip-172-31-35-224.ec2.internal>
2021-03-11 23:32:17 +0000

    [Finish] 2-4

--- ruby/lesson4.rb
+++ ruby/lesson4.rb
@@ -1,2 +1,11 @@
 name = "福田哲也"
 puts name
+
+$animal = "inu" #グローバル変数　プロジェクト内のどこでも使用できる。
+@fish = "tinu" #インスタンス変数　クラス内の全メソッド（どこでも）使用できる
+bird = "kakko" #ローカル変数　宣言したメソッド・ブロック内でのみ使用する。
+
+puts $animal
+puts @fish
+puts bird
+
