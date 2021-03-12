5b6fa2d0ea9868350017572ee6a92ddc3a2b2971
EC2 Default User <ec2-user@ip-172-31-35-224.ec2.internal>
2021-03-12 01:23:23 +0000

    [Finish] 2-6

---
+++ ruby/total_price.rb
@@ -0,0 +1,17 @@
+total =50
+
+if total < 100
+  puts "みかんを購入することができません"
+end
+
+total = total + 50
+
+if total = 100
+  puts "みかんを購入。所持金は0円。"
+end
+
+total = total + 50
+
+if total > 100
+  puts "みかんを購入。所持金に余りあり。"
+end
