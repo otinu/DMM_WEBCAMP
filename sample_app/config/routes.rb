71d5aaf1cb05379e8a3fb390ba8fa7ba7b6e9dd1
EC2 Default User <ec2-user@ip-172-31-35-224.ec2.internal>
2021-03-17 01:49:52 +0000

    [Finish] 2-9

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #一見、todolistsが重複しているように見えるが注視すると、HTTPメソッドとURLが完全に一致しているものは一つもない
   get 'todolists/new'
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
