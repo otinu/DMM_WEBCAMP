8db6155213de9e4b19ae4b007cf5994f7d2b4535
EC2 Default User <ec2-user@ip-172-31-35-224.ec2.internal>
2021-04-08 12:09:02 +0000

    [Finish]JS-13

--- skipper_app/app/assets/javascripts/application.js
+++ skipper_app/app/assets/javascripts/application.js
@@ -10,7 +10,35 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+let title = "javascriptが使えました";
+alert(title);
+
+$(function () {
+ $('.jquery').on('click', function() {
+  $(this).css('color','red');
+ });
+});
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    transition : 'slide',          // スライドショーの変化 ("fade" or "slide")
+    speed : 1000,                  // 変化に係る時間(ミリ秒)
+    easing : 'easeOutQuart',       // easingの種類
+    navType : 'block',             // ナビゲーションの形("block" or "bubble")
+    childrenElementType : 'div',   // 子要素の種類('div' or 'img')
+    arrows : true,                 // ナビゲーション矢印の表示(trueで表示)
+    autoPlay : true,               // スライドショーの自動再生(falseで自動再生なし)
+    autoPlayDuration : 3000,       // 自動再生時のスライド切替間隔(ミリ秒)
+    keyboardOnAlways : true,       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    hidePrevious : false           // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+  });
+});
