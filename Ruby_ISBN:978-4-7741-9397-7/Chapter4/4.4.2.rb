c5da38ca375aac4b0654444850c8a9765c160bb8
otinu <fitshter@gmail.com>
2021-04-14 11:01:50 +0900

    save

---
+++ Ruby_ISBN:978-4-7741-9397-7/Chapter4/4.4.2.rb
@@ -0,0 +1,12 @@
+puts "【select】メソッド"
+puts "各要素に対して判定を行い、真の要素だけを集めた新たな配列を返す。"
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = numbers.select { |f| f.even? }
+p even_numbers
+
+puts "【reject】メソッド"
+puts "selectメソッドと真逆。"
+puts "各要素に対して判定を行い、の要素だけを集めた新たな配列を返す。"
+array = [1, 2, 3, 4, 5, 6]
+non_multiples_of_thhree = numbers.reject { |f| f % 3 == 0 }
+p non_multiples_of_thhree
