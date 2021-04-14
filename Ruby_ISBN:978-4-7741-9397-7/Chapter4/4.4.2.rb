4841a9775d18352bcd440c78582fefd5555a2b67
otinu <fitshter@gmail.com>
2021-04-14 16:32:12 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/Chapter4/4.4.2.rb
+++ Ruby_ISBN:978-4-7741-9397-7/Chapter4/4.4.2.rb
@@ -6,7 +6,7 @@ p even_numbers
 
 puts "【reject】メソッド"
 puts "selectメソッドと真逆。"
-puts "各要素に対して判定を行い、の要素だけを集めた新たな配列を返す。"
+puts "各要素に対して判定を行い、偽の要素だけを集めた新たな配列を返す。"
 array = [1, 2, 3, 4, 5, 6]
 non_multiples_of_thhree = numbers.reject { |f| f % 3 == 0 }
 p non_multiples_of_thhree
