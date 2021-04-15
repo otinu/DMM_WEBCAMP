0dc33c211b82e50a0e18b4ef8c327c81b162f0b9
otinu <fitshter@gmail.com>
2021-04-15 16:38:11 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/ruby-book/lib/convert_length.rb
+++ Ruby_ISBN:978-4-7741-9397-7/ruby-book/lib/convert_length.rb
@@ -1,4 +1,6 @@
-def convert_length(length, unit_from, unit_to)
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
+
+def convert_length(length, from: :m, to: :m)
   units = { m: 1.0, ft: 3.28, in: 39.37 }
-  (length / units[unit_from] * units[unit_to]).round(2)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
