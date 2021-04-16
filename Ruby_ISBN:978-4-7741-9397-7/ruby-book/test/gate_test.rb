af0a8c8903967ce1136193a0440d59a92b0c7cc7
otinu <fitshter@gmail.com>
2021-04-16 17:13:19 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/ruby-book/test/gate_test.rb
+++ Ruby_ISBN:978-4-7741-9397-7/ruby-book/test/gate_test.rb
@@ -3,25 +3,37 @@ require "./lib/gate"
 require "./lib/ticket"
 
 class GateTest < Minitest::Test
-  
+
   def setup
     @umeda = Gate.new(:umeda)
     @juso = Gate.new(:juso)
     @mikuni = Gate.new(:mikuni)
   end
-  
+
   def test_umeda_to_juso
     ticket = Ticket.new(150)  #Ticketオブジェクトの生成が可能かテスト
     @umeda.enter(ticket)      #梅田から入場できるかテスト
     assert @juso.exit(ticket) #十三で降りることができるかテスト
   end
-  
+
   def test_umeda_to_mikuni_when_fare_is_not_enough
     umeda = Gate.new(:umeda)
     mikuni = Gate.new(:mikuni)
-    
+
     ticket = Ticket.new(150)
     @umeda.enter(ticket)
     refute @mikuni.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
