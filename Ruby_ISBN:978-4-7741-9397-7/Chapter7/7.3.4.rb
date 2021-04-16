8211c61a28e8a1a8d7dfdb2d284f7e8129a73a38
otinu <fitshter@gmail.com>
2021-04-16 15:28:17 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/Chapter7/7.3.4.rb
+++ Ruby_ISBN:978-4-7741-9397-7/Chapter7/7.3.4.rb
@@ -4,6 +4,8 @@ class User
   end
 
   # self.を付けるとクラスメソッドになる
+  #クラスメソッドはインスタンス変数(インスタンスに含まれるデータ)を使わない場合に定義。
+  #インスタンス変数使わないVer インスタンス
   def self.create_users(names)
     names.map do |name|
       User.new(name)
@@ -17,9 +19,15 @@ class User
 end
 
 names = ["Alice", "Bob", "Carol"]
-#クラスメソッドの呼び出し
+
+#==================================ここまでが、諸々の定義=============
+
+
+#クラスメソッドを呼び出して、usersに格納
 users = User.create_users(names)
+p users
 users.each do |user|
   #インスタンスメソッドの呼び出し
+  #インスタンスメソッドはインスタンス変数(インスタンスに含まれるデータ)を読み書きする場合に活用する。
   puts user.hello
 end
