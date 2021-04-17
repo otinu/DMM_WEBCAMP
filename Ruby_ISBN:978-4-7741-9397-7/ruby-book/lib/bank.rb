f3f010a28d06e4284758286a33cc2365519575ff
otinu <fitshter@gmail.com>
2021-04-17 17:24:32 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/ruby-book/lib/bank.rb
+++ Ruby_ISBN:978-4-7741-9397-7/ruby-book/lib/bank.rb
@@ -3,5 +3,5 @@ require "./lib/deep_freezable"
 class Bank
   extend DeepFreezable
 
-  CURRENCIES = deep_freeze({ "Japan" => "yean", "US" => "dollar", "India" => "rupee"})
+  CURRENCIES = deep_freeze({ "Japan" => "yen", "US" => "dollar", "India" => "rupee"})
 end
