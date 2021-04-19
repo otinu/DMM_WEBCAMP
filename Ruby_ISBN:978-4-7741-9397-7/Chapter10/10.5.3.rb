53258828f28f6717108dc714f37068323e93f38e
otinu <fitshter@gmail.com>
2021-04-19 10:33:39 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/Chapter10/10.5.3.rb
+++ Ruby_ISBN:978-4-7741-9397-7/Chapter10/10.5.3.rb
@@ -1,6 +1,6 @@
 def generate_proc(array)
   counter = 0
-  Proc.new do
+  Proc.new do #ここで、Procオブジェクトを生成
     counter += 10
     array << counter #配列でないとこの処理はできない。
   end
@@ -9,9 +9,21 @@ end
 values = []
 sample_proc = generate_proc(values)
 
+a = <<"EXPLAIN"
+ローカル変数valuesはメソッドの終了とともに、格納された値が解放(?)される。
+そのため、valuesの中身は空となり、元々与えられていた配列の形[]だけが返る。 \n
+EXPLAIN
 
+puts a
+p values
+puts " \n "
 
-p values　#この行はなくても実行できる。
+b = <<"COMMENT"
+しかし、Procオブジェクトでは格納された値は解放(?)されずに残っている。
+そのため、Procオブジェクトを呼び続けると、配列にどんどん値が追加されていく。 \n
+COMMENT
+
+puts b
 
 sample_proc.call
 p values
