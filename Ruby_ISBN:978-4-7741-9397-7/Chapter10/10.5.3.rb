4b0caa2c8f0430388c08303f963b53e26d2eae71
otinu <fitshter@gmail.com>
2021-04-19 10:22:49 +0900

    save

--- Ruby_ISBN:978-4-7741-9397-7/Chapter10/10.5.3.rb
+++ Ruby_ISBN:978-4-7741-9397-7/Chapter10/10.5.3.rb
@@ -2,13 +2,16 @@ def generate_proc(array)
   counter = 0
   Proc.new do
     counter += 10
-    array << counter
+    array << counter #配列でないとこの処理はできない。
   end
 end
 
 values = []
 sample_proc = generate_proc(values)
-p values
+
+
+
+p values　#この行はなくても実行できる。
 
 sample_proc.call
 p values
